51466e78184a38a8056126d284c1a0125eca4c21
tpmccrary <timothypmccrary@gmail.com>
2021-02-22 16:40:27 -0700

    Added inital structure of project

---
+++ AI_Path_Finding/map_grid.py
@@ -0,0 +1,13 @@
+class MapGrid:
+    height = 0
+    width = 0
+    startLoc = []
+    goalLoc = []
+    grid = []
+
+    def __init__(self) -> None:
+        pass
+
+
+    def readMapFile():
+        return
---
+++ ai_path_finding.py
@@ -0,0 +1,8 @@
+import AI_Path_Finding
+
+def main():
+    print("test")
+
+
+if __name__ == '__main__':
+    main()
