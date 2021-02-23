3130e7572fe31067544bb47cc1666131d07ccd2d
tashicn <60363140+tashicn@users.noreply.github.com>
2021-02-22 21:19:20 -0700

    Create BFS.py

---
+++ AI_Path_Finding/BFS.py
@@ -0,0 +1,25 @@
+class BFS:
+    
+    def BFS(map, start, end):
+        explored = []
+        queue = [[start]]
+
+        if start == goal:
+           return
+
+        while queue:
+            path = queue.pop()
+            node = path[-1]
+
+            if node is not visited:
+                neighbors = map[node]
+
+                for neighbors in neighbors:
+                    new_path = list (path)
+                    new_path.append(neighbor)
+                    queue.append(new_path)
+
+                    if neighbor == goal:
+                        print("shortest path = ", *new_path)
+                        return 
+                visited(append)
