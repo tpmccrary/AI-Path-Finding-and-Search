c695416388f5b85f9914abc138ac55035bfd36c6
tpmccrary <timothypmccrary@gmail.com>
2021-02-22 21:03:36 -0700

    Added function that gets data from map file

--- AI_Path_Finding/map_grid.py
+++ AI_Path_Finding/map_grid.py
@@ -1,22 +1,41 @@
 class MapGrid:
-    height = 0
-    width = 0
-    startLoc = []
-    goalLoc = []
-    grid = []
+    def __init__(self, mapFile):
+        self.height = 0
+        self.width = 0
+        self.startLoc = []
+        self.goalLoc = []
+        self.grid = []
+        self.readMapFile(mapFile)
 
-    def __init__(self) -> None:
-        pass
-
-    @staticmethod
-    def readMapFile(mapFile):
+    # Given a map file, record its data.
+    def readMapFile(self, mapFile):
         if (mapFile == None or mapFile == ""):
             print("No file given. Exiting...")
             exit(1)
         print("Reading map file...")
 
+        lineNum = 0
+        # We go through every line in the data.
         for line in mapFile:
-            print(line)
-            
+            # If it is the first line, get the heigh and width.
+            if (lineNum == 0):
+                splitLine = line.split()
+                self.height = splitLine[0]
+                self.width = splitLine[1]
+            # If it is the seconds line, get the starting location.
+            elif (lineNum == 1):
+                splitLine = line.split()
+                self.startLoc.append(splitLine[0])
+                self.startLoc.append(splitLine[1])
+            # If it is the third line, get the goal location.
+            elif (lineNum == 2):
+                splitLine = line.split()
+                self.goalLoc.append(splitLine[0])
+                self.goalLoc.append(splitLine[1])
+            elif (lineNum >= 3):
+                splitLine = line.split()
+                self.grid.append(splitLine)
 
+            lineNum += 1
+        
         return
--- ai_path_finding.py
+++ ai_path_finding.py
@@ -3,12 +3,11 @@ import AI_Path_Finding
 import fileinput
 
 def main():
-    # Create new map grid object.
-    mapGrid = MapGrid()
     # Get the file from the user input.
     mapFile = fileinput.input()
-    # Read the file to grab the details.
-    mapGrid.readMapFile(mapFile)
+    # Create new map grid object.
+    mapGrid = MapGrid(mapFile)
+    
 
 
 
