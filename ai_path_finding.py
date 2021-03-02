29962b540be1c0e530ad066705cff554c475f7d0
tpmccrary <timothypmccrary@gmail.com>
2021-03-02 02:29:50 -0700

    Update README

--- ai_path_finding.py
+++ ai_path_finding.py
@@ -10,7 +10,8 @@ def main():
     if (len(sys.argv) != 3):
         print("Incorrect commands.\nPlease enter: <filepath>.txt <searching_algorithm>\nExiting...")
         sys.exit(1)
-
+    
+    # TODO: Add logic that checks if arg is in list. This allows the user to run multiple args. if "BFS" in sys.args:
     # Get the arguments from user.
     fileName = sys.argv[1]
     algorithm = sys.argv[2]
